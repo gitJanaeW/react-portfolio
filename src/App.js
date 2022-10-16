d7ed030d9a3bc9298fe746d29ff2c73d7f1cd928
janae.wel <janae.wel@gmail.com>
2022-10-16 16:11:26 -0400

    added clicked navbar effect

--- src/App.js
+++ src/App.js
@@ -44,3 +44,74 @@ function App() {
 }
 
 export default App;
+
+// whats' not working
+
+
+// const [clickedAbout, setAbout] = useState(true);
+// const [clickedPortfolio, setPortfolio] = useState(false);
+// const [clickedResume, setResume] = useState(false);
+// const [clickedContact, setContact] = useState(false);
+
+// const handleNavClick = (e) => {
+//   e.preventDefault();
+//   switch (e.target.innerHTML) {
+//       case "About":
+//           setAbout(true);
+//           setPortfolio(false);
+//           setResume(false);
+//           setContact(false);
+//           break;
+//       case "Portfolio":
+//           setAbout(false);
+//           setPortfolio(true);
+//           setResume(false);
+//           setContact(false);
+//           break;
+//       case "Contact":
+//           setAbout(false);
+//           setPortfolio(false);
+//           setResume(false);
+//           setContact(true);
+//           break;
+//       case "Resume":
+//           setAbout(false);
+//           setPortfolio(false);
+//           setResume(true);
+//           setContact(false);
+//           break;
+//       default:
+//           break;
+//   }
+// };
+
+{/* <Link to="/">
+                    <h1 className="janaeWelsh">JANAE WELSH</h1>
+                </Link>
+                <Link to="/">
+                    <p data-testid="about"
+                    onClick={handleNavClick}
+                    className={`${clickedAbout && "selected"}`}>
+                        About Me
+                    </p>
+                </Link>
+                <Link to="/portfolio"
+                    onClick={handleNavClick}
+                    className={`${clickedPortfolio && "selected"}`}>
+                    <p data-testid="portfolio">
+                        Portfolio
+                    </p>
+                </Link>
+                <Link to="/contact">
+                    <p data-testid="contact"
+                    onClick={handleNavClick} className={`${clickedContact && "selected"}`}>
+                        Contact
+                    </p>
+                </Link>
+                <Link to="/resume">
+                    <p data-testid="resume"
+                    onClick={handleNavClick}
+                    className={`${clickedResume && "selected"}`}>
+                        Resume
+                    </p>
+                </Link> */}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from 'react-router-dom';
 
 const Nav = () => {
+    const [clickedAbout, setAbout] = useState(true);
+    const [clickedPortfolio, setPortfolio] = useState(false);
+    const [clickedContact, setContact] = useState(false);
+    const [clickedResume, setResume] = useState(false);
+    const toggleNav = (e) => {
+        e.preventDefault();
+        // const selected = document.getElementsByClassName('selected');
+        console.log(e.target.innerHTML);
+        switch (e.target.innerHTML) {
+            case "About Me":
+                setAbout(true);
+                setPortfolio(false);
+                setResume(false);
+                setContact(false);
+                break;
+            case "Portfolio":
+                setAbout(false);
+                setPortfolio(true);
+                setResume(false);
+                setContact(false);
+                break;
+            case "Contact":
+                setAbout(false);
+                setPortfolio(false);
+                setResume(false);
+                setContact(true);
+                break;
+            case "Resume":
+                setAbout(false);
+                setPortfolio(false);
+                setResume(true);
+                setContact(false);
+                break;
+            default:
+                console.log('break');
+                break;
+        }
+    }
     return (
-        <div>
-            <div className="navbar">
-                <Link to="/"><h1 className="janaeWelsh">JANAE WELSH</h1></Link>
-                <Link to="/"><p data-testid="about">About Me</p></Link>
-                <Link to="/portfolio"><p data-testid="portfolio">Portfolio</p></Link>
-                <Link to="/contact"><p data-testid="contact">Contact</p></Link>
-                <Link to="/resume"><p data-testid="resume">Resume</p></Link>
+        <div onClick={toggleNav}>
+            <div className="navbar" >
+                <Link to="/">
+                    <h1 className="janaeWelsh">JANAE WELSH</h1>
+                </Link>
+                <Link to="/">
+                    <p data-testid="about" className={`${clickedAbout && "selected"}`}>About Me</p>
+                </Link>
+                <Link to="/portfolio">
+                    <p data-testid="portfolio" className={`${clickedPortfolio && "selected"}`}>Portfolio</p>
+                </Link>
+                <Link to="/contact">
+                    <p data-testid="contact" className={`${clickedContact && "selected"}`}>Contact</p>
+                </Link>
+                <Link to="/resume">
+                    <p data-testid="resume" className={`${clickedResume && "selected"}`}>Resume</p>
+                </Link>
             </div>
         </div>
     );
 };
 
 export default Nav;
+
+// WHAT'S NOT WORKING:
